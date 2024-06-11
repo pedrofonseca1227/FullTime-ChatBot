bf8c3b3ee4208d556f3f7f716a8691e94ed8a245
pedrofonseca1227 <152933807+pedrofonseca1227@users.noreply.github.com>
2024-06-11 18:54:38 -0300

    Add files via upload

---
+++ ChatGPT-APIS-master/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// Vuetify
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+// app.use(router)
+
+// app.mount('#app')
